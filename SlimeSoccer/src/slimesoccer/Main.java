db44e843bc786cc182db15fc45756156658f0d53
martinkamleithner.schule@gmail.com <martinkamleithner.schule@gmail.com@5c779398-4ef5-3051-1d0d-0e3663510683>
2011-05-19 18:44:29 +0000

    Vector2D v1.0, mit add, subtract changeXDir changeYDir einheitsVector lenght squarelength toString getter, setter Construktoren

---
+++ SlimeSoccer/src/slimesoccer/Main.java
@@ -0,0 +1,26 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package slimesoccer;
+
+/**
+ *
+ * @author TicTacMoe
+ */
+public class Main {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        System.out.println("Revision 2 test :D");
+        //diverse Tests
+        Vector2D v = new Vector2D(4,0);
+        v.einheitsVector();
+        System.out.println(v);
+        
+    }
+
+}
--- SlimeSoccer/src/slimesoccer/Vector2D.java
+++ SlimeSoccer/src/slimesoccer/Vector2D.java
@@ -8,25 +8,64 @@ public class Vector2D {
 
     private double x, y; //X is ume, Y is aufe
 
-    public Vector2D() {
-        this(0, 0);
+
+    /**
+     * Addiert den übergebenen Vektor.
+     * @param v2 
+     */
+      public void add(Vector2D v2) {
+        this.x += v2.x;
+        this.y += v2.y;
     }
+
 
     public Vector2D(int x, int y) {
         this.x = x;
         this.y = y;
     }
 
-    public void add(Vector2D v2) {
-        this.x += v2.x;
-        this.y += v2.y;
-    }
 
+  
+
+
+    /**
+     * Subtrahiert den übergeben Vektor.
+     * @param v2 
+     */
+    public void subtract(Vector2D v2) {
+        this.x -= v2.x;
+        this.y -= v2.y;
+
+    }
+    
+    /**
+     * Liefert die Länge des Vektors.
+     * @return 
+     */
     public double length() {
         return Math.sqrt(x * x + y * y);
     }
-
-    public void einheitsVector() {
+    /**
+     * Liefert die quadratische Länge des Vektors (aus Performancegründen da sqrt langsam ist)
+     * @return 
+     */
+    public double squarelength() {
+        return (x * x + y * y);
+    }
+    
+    /**
+     * Setzt den Vector auf seinen Einheitsvektor. 
+     * 
+     * @return Eineheitsvektor (this) 
+     */
+    public Vector2D einheitsVector() {
+        double l=this.length();
+        this.x/=l;
+        this.y/=l;
+        //Länge muss 1 sein
+        assert(Math.abs(this.length()-1)<1E-5);
+        return this;
+        
     }
 
     public void changeXDir() {
@@ -52,4 +91,29 @@ public class Vector2D {
     public void setY(double y) {
         this.y = y;
     }
+
+    public Vector2D(double x, double y) {
+        this.x = x;
+        this.y = y;
+    }
+    /**
+     * Erzeugt einen neuen Vektor mit x und y auf 0.
+     */
+    public Vector2D(){
+        
+    }
+    /**
+     * Erzeugt einen neuen Vector, der eine Kopie des übergebenen ist.
+     * @param anotherVector 
+     */
+    public Vector2D(Vector2D anotherVector) {
+        this.x = anotherVector.x;
+        this.y = anotherVector.y;
+    }
+    
+    @Override
+    public String toString(){
+        return String.format("X: %.3f, Y: %.3f, Länge: %.3f", x,y,length());
+    }
+            
 }
